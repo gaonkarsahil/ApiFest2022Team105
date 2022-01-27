f4f0abf6b348f35cd8ab70766f5aeba834142eda
DivyanshSareen <sareendivyansh@gmail.com>
2022-01-27 20:55:43 +0530

    wired up the js for list of cards
    the cards with pet details now show all the mock values

--- Frontend/search.js
+++ Frontend/search.js
@@ -11,11 +11,20 @@ const grid = document.getElementsByClassName("card-title");
 
 const func = (pets) => {
     const cardImage = document.getElementsByClassName("card-image");
-    console.log();
+    const cardContent = document.getElementsByClassName("card-content");
+    console.log(
+        );
     for(let i = 0; i < 10; i+=1)
     {
-        cardImage[i].children[0].attributes["src"].value = pets[i]["images"]["0"]
-        // console.log(pets[i]["name"], pets[i]["animal"], pets[i]["breed"], pets[i]["state"], pets[i]["city"], pets[i]["description"], pets[i]["images"])
+        // pet image
+        cardImage[i].children[0].attributes["src"].value = pets[i]["images"]["0"];
 
+        // pet details
+        cardImage[i].children[1].innerText = `${pets[i]["name"]} | ${pets[i]["animal"]}\n${pets[i]["breed"]} | ${pets[i]["city"]} ${pets[i]["state"]}`;
+        
+        // pet descripion
+        cardContent[i].children[0].innerText = pets[i]["description"]
+
+        // console.log(pets[i]["name"], pets[i]["animal"], pets[i]["breed"], pets[i]["state"], pets[i]["city"], pets[i]["description"], pets[i]["images"])
     }
 }
